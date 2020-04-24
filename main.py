b82d269e819cf25d0f71297c3ea3c3c43fcedb99
mockingbird12 <mihazyx@yandex.ru>
2020-04-24 22:09:20 +0300

    Добавляю диалоги и кнопки управления.

--- main.py
+++ main.py
@@ -6,15 +6,42 @@ import time
 
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler(commands='logged_user')
+@bot.message_handler(commands=['coin_count'])
+def coin_count(message):
+    bot.send_message(message.chat.id, conversation.coin_count)
+
+@bot.message_handler(commands=['chose_coin'])
+def chouse_coin(message):
+    bot.send_message(message.chat.id, conversation.chouse_coin)
+
+@bot.message_handler(commands=['sell_coin'])
+def sell_coin(message):
+    bot.send_message(message.chat.id, conversation.sell_coin)
+
+
+@bot.message_handler(commands=['buy_coin'])
+def buy_coin(message):
+    bot.send_message(message.chat.id, conversation.buy_coin)
+
+
+@bot.message_handler(commands=['watch_portfolio'])
+def watch_portfolio(message):
+    bot.send_message(message.chat.id, conversation.watch_portfolio)
+
+
+@bot.message_handler(commands=['watch_course'])
+def watch_course(message):
+    bot.send_message(message.chat.id, conversation.watch_course)
+
+@bot.message_handler(commands=['loged_user'])
 def logged_user(message):
     bot.send_message(message.chat.id, conversation.hello_logged)
 
-@bot.message_handler(commands='unloged_user')
+@bot.message_handler(commands=['unloged_user'])
 def unloged_user(message):
     bot.send_message(message.chat.id, conversation.hello_unlogged)
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(commands=['start'])
 def main_start(message):
     bot.send_message(message.chat.id, conversation.hello_message)
 
@@ -22,9 +49,4 @@ def main_start(message):
 if __name__ == '__main__':
     telebot.apihelper.proxy = {'https': 'socks5h://192.168.77.130:9100'}
     print('Start Bot')
-    while True:
-        try:
-            bot.polling()
-        except Exception:
-            print('Error')
-            time.sleep(5)
+    bot.polling()
