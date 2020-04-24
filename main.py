20f8b144263ead14889bc5b782e442034c39d2a4
mockingbird12 <mihazyx@yandex.ru>
2020-04-24 19:39:42 +0300

    Добавляю диалоги.

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import telebot
 from telebot import apihelper
+import conversation
 import config
 import time
 
 bot = telebot.TeleBot(config.token)
 
+@bot.message_handler(commands='logged_user')
+def logged_user(message):
+    bot.send_message(message.chat.id, conversation.hello_logged)
+
+@bot.message_handler(commands='unloged_user')
+def unloged_user(message):
+    bot.send_message(message.chat.id, conversation.hello_unlogged)
+
 @bot.message_handler(content_types=['text'])
 def main_start(message):
-    hello_message = 'Hello'
-    bot.send_message(message.chat.id, hello_message)
+    bot.send_message(message.chat.id, conversation.hello_message)
 
 
 if __name__ == '__main__':
