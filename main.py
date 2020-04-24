20bce3257145f878bc22c91a9be941286fa02acb
mockingbird12 <mihazyx@yandex.ru>
2020-04-24 18:02:44 +0300

    Добавил библиотеки, необходимые для запуска бота.

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import telebot
 from telebot import apihelper
 import config
+import time
 
 bot = telebot.TeleBot(config.token)
 
+@bot.message_handler(content_types=['text'])
+def main_start(message):
+    hello_message = 'Hello'
+    bot.send_message(message.chat.id, hello_message)
+
 
 if __name__ == '__main__':
-    telebot.apihelper.proxy = {'https': 'socks5://192.168.77.130:9050'}
+    telebot.apihelper.proxy = {'https': 'socks5://192.168.77.130:9100'}
     print('Start Bot')
-    bot.polling()
+    while True:
+        try:
+            bot.polling()
+        except Exception:
+            print('Error')
+            time.sleep(5)
