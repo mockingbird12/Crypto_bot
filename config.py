a0810b58896f2d754abb89c3ab79eaf450488ce9
mockingbird12 <mihazyx@yandex.ru>
2020-10-11 22:36:16 +0300

    11.10 разобрался с Crypto_value.

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 
 db_user = 'crypto_user'
 db_passwd = '111111'
-db_host = '192.168.77.130'
-# db_host = '172.17.0.1' # docker interface
+# db_host = '192.168.77.130'
+db_host = '172.17.0.1' # docker interface
 db_name = 'crypto_bot'
 token = '1238244677:AAE0CQraYtpWvooXUVbgfp9tO1l_bartmx0'
 
